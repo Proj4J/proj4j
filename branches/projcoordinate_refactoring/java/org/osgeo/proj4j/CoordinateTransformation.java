cf85fb655ad2d094283733fb2cd2b99bc37b2c1c
surveyor <surveyor@4e78687f-474d-0410-85f9-8d5e500ac6b2>
2009-12-14 19:37:26 +0000

    Creating a branch for refactoring needed to integrate ProjCoordinate class into the library.
    git-svn-id: http://svn.osgeo.org/metacrs/proj4j@1719 4e78687f-474d-0410-85f9-8d5e500ac6b2

---
+++ branches/projcoordinate_refactoring/java/org/osgeo/proj4j/CoordinateTransformation.java
@@ -0,0 +1,180 @@
+package org.osgeo.proj4j;
+
+import java.awt.geom.Point2D;
+
+import org.osgeo.proj4j.datum.GeocentricConverter;
+
+/**
+ * Represents the operation of transforming 
+ * a {@link ProjCoordinate} from its current {@link CoordinateSystem} 
+ * into a different one.
+ * The transformation involves the following steps:
+ * <ul>
+ * <li>If the input coordinate is in a projection coordinate system,
+ * it is inverse-projected into a geographic coordinate 
+ * <li>If the source and destination datums are different,
+ * the geographic coordinate is converted from the source to the destination datum
+ * as accurately as possible
+ * <li>If the destination coordinate system is a projection, 
+ * the geographic coordinate is projected into it.
+ * <ul>
+ * <p>
+ * A coordinate transformation is stateful, and thus is not thread-safe.
+ * However, it may be reused any number of times within a single thread.
+ * <p>
+ * Details of the transformation procedure are pre-computed
+ * and cached in this object for efficiency
+ * in computing tranformations.
+ * 
+ * @author Martin Davis
+ *
+ */
+public class CoordinateTransformation 
+{
+	private CoordinateSystem srcCS;
+	private CoordinateSystem destCS;
+	
+  // temporary variable for intermediate results
+	private Point2D.Double geoPt = new Point2D.Double();
+  private ProjCoordinate geoCoord = new ProjCoordinate(0,0);
+	
+  // precomputed information
+	private boolean doInverseProjection = true;
+	private boolean doForwardProjection = true;
+  private boolean doDatumTransform = false;
+  private boolean transformViaGeocentric = false;
+  private GeocentricConverter srcGeoConv; 
+  private GeocentricConverter destGeoConv; 
+	
+  /**
+   * Creates a transformation from a source {@link CoordinateSystem} to a 
+   * destination one.
+   * 
+   * @param srcCS
+   * @param destCS
+   */
+	public CoordinateTransformation(CoordinateSystem srcCS, CoordinateSystem destCS)
+	{
+		this.srcCS = srcCS;
+		this.destCS = destCS;
+		
+		// compute strategy for transformation at initialization time, to make transformation more efficient
+		// this may include precomputing sets of parameters
+		
+		doInverseProjection = (srcCS != null && srcCS != CoordinateSystem.CS_GEO);
+		doForwardProjection = (destCS != null && destCS != CoordinateSystem.CS_GEO);
+    doDatumTransform = doInverseProjection && doForwardProjection
+      && srcCS.getDatum() != destCS.getDatum();
+    
+    if (doDatumTransform) {
+      
+      boolean isEllipsoidEqual = srcCS.getEllipsoid().isEqual(destCS.getEllipsoid());
+      if (! isEllipsoidEqual) 
+          transformViaGeocentric = true;
+      if (srcCS.getDatum().isTransform() 
+          || destCS.getDatum().isTransform())
+          transformViaGeocentric = true;
+      
+      if (transformViaGeocentric) {
+        srcGeoConv = new GeocentricConverter(srcCS.getEllipsoid());
+        destGeoConv = new GeocentricConverter(destCS.getEllipsoid());
+      }
+    }
+	}
+	
+	/**
+   * Tranforms a coordinate from the source {@link CoordinateSystem} 
+   * to the destination one.
+   * 
+   * @param src the input coordinate
+   * @param dest the transformed coordinate
+   * @return the destination coordinate which was passed in
+   * 
+   * @throws ProjectionException if a computation error is encountered
+	 */
+	public Point2D.Double transform( Point2D.Double src, Point2D.Double dest )
+  throws ProjectionException
+	{
+		// NOTE: this method may be called many times, so needs to be as efficient as possible
+    
+		if (! doInverseProjection) {
+			geoPt.x = src.x;
+			geoPt.y = src.y;
+		}
+		else {
+			// inverse project to geo
+			srcCS.getProjection().inverseTransformRadians(src, geoPt);
+		}
+		
+    //TODO: adjust src Prime Meridian if specified
+    
+    if (doDatumTransform) {
+      geoCoord.x = geoPt.x;
+      geoCoord.y = geoPt.y;
+      geoCoord.z = 0.0; // for now
+      datumTransform(geoCoord);
+      geoPt.x = geoCoord.x;
+      geoPt.y = geoCoord.y;
+      // ignore Z for now
+    }
+		
+    //TODO: adjust dest Prime Meridian if specified
+
+		if (! doForwardProjection) {
+			dest.x = geoPt.x;
+			dest.y = geoPt.y;
+		}
+		else {
+			// project from geo
+			destCS.getProjection().transformRadians(geoPt, dest);
+		}
+		return dest;
+	}
+  
+  /**
+   * 
+   * Input:  long/lat/z coordinates in radians in the source datum
+   * Output: long/lat/z coordinates in radians in the destination datum
+   * 
+   * @param geoPt the point containing the input and output values
+   */
+  private void datumTransform(ProjCoordinate pt)
+  {
+    /* -------------------------------------------------------------------- */
+    /*      Short cut if the datums are identical.                          */
+    /* -------------------------------------------------------------------- */
+    if (srcCS.getDatum().isEqual(destCS.getDatum()))
+      return;
+    
+    // TODO: grid shift if required
+    
+    /* ==================================================================== */
+    /*      Do we need to go through geocentric coordinates?                */
+    /* ==================================================================== */
+    if (transformViaGeocentric) {
+      /* -------------------------------------------------------------------- */
+      /*      Convert to geocentric coordinates.                              */
+      /* -------------------------------------------------------------------- */
+      srcGeoConv.convertGeodeticToGeocentric( pt );
+      
+      /* -------------------------------------------------------------------- */
+      /*      Convert between datums.                                         */
+      /* -------------------------------------------------------------------- */
+      if( srcCS.getDatum().isTransform() ) {
+        srcCS.getDatum().transformFromGeocentricToWgs84( pt );
+      }
+      if( destCS.getDatum().isTransform() ) {
+        destCS.getDatum().transformToGeocentricFromWgs84( pt );
+      }
+
+      /* -------------------------------------------------------------------- */
+      /*      Convert back to geodetic coordinates.                           */
+      /* -------------------------------------------------------------------- */
+      destGeoConv.convertGeocentricToGeodetic( pt );
+    }
+    
+    // TODO: grid shift if required
+
+  }
+  
+}
---
+++ branches/projcoordinate_refactoring/java/org/osgeo/proj4j/Datum.java
@@ -0,0 +1,180 @@
+/*
+Copyright 2006 Jerry Huxtable
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package org.osgeo.proj4j;
+
+
+/**
+ * A geodetic datum. 
+ */
+public class Datum 
+{
+  public static final int TYPE_UNKNOWN = 0;
+  public static final int TYPE_WGS84 = 1;
+  public static final int TYPE_3PARAM = 2;
+  public static final int TYPE_7PARAM = 3;
+  public static final int TYPE_GRIDSHIFT = 4;
+  
+  public static final Datum WGS84 = new Datum("WGS84", 0,0,0, Ellipsoid.WGS84, "WGS84"); 
+  public static final Datum GGRS87 = new Datum("GGRS87", -199.87,74.79,246.62, Ellipsoid.GRS80, "Greek_Geodetic_Reference_System_1987");
+  public static final Datum NAD83 = new Datum("NAD83", 0,0,0, Ellipsoid.GRS80,"North_American_Datum_1983");
+  //public static final Datum NAD27 = new Datum("NAD27", "@conus,@alaska,@ntv2_0.gsb,@ntv1_can.dat", Ellipsoid.CLARKE_1866,"North_American_Datum_1927");
+  public static final Datum POTSDAM = new Datum("potsdam", 606.0,23.0,413.0, Ellipsoid.BESSEL, "Potsdam Rauenberg 1950 DHDN");
+  public static final Datum CARTHAGE = new Datum("carthage",-263.0,6.0,431.0, Ellipsoid.CLARKE_1880, "Carthage 1934 Tunisia");
+  public static final Datum HERMANNSKOGEL = new Datum("hermannskogel", 653.0,-212.0,449.0, Ellipsoid.BESSEL, "Hermannskogel");
+  public static final Datum IRE65 = new Datum("ire65", 482.530,-130.596,564.557,-1.042,-0.214,-0.631,8.15, Ellipsoid.MOD_AIRY, "Ireland 1965");
+  public static final Datum NZGD49 = new Datum("nzgd49", 59.47,-5.04,187.44,0.47,-0.1,1.024,-4.5993, Ellipsoid.INTERNATIONAL, "New Zealand Geodetic Datum 1949");
+  public static final Datum OSEB36 = new Datum("OSGB36", 446.448,-125.157,542.060,0.1502,0.2470,0.8421,-20.4894, Ellipsoid.AIRY, "Airy 1830");
+
+  String code;
+	String name;
+	Ellipsoid ellipsoid;
+	double[] transform;
+	
+  public Datum(String code, 
+      double deltaX, double deltaY, double deltaZ, 
+      Ellipsoid ellipsoid,
+      String name) {
+    this(code, new double[] { deltaX, deltaY, deltaZ },ellipsoid, name);
+  }
+  
+  public Datum(String code, 
+      double deltaX, double deltaY, double deltaZ,
+      double rx, double ry, double rz, double mbf,
+      Ellipsoid ellipsoid,
+      String name) {
+    this(code, new double[] { deltaX, deltaY, deltaZ, rx, ry, rx, mbf },ellipsoid, name);
+  }
+  
+  public Datum(String code, 
+      double[] transform, 
+      Ellipsoid ellipsoid,
+      String name) {
+    this.code = code;
+    this.name = name;
+    this.ellipsoid = ellipsoid;
+    this.transform = transform;
+  }
+  
+  public String getCode() { return code; }
+  
+  public double[] getTransform()
+  {
+    return transform;
+  }
+  
+  public int getType()
+  {
+    if (transform  == null) return TYPE_WGS84;
+    
+    //TODO: check for transform = all zeroes and return TYPE_WGS84 if so
+    
+    if (transform.length  == 3) return TYPE_3PARAM;
+    if (transform.length  == 7) return TYPE_7PARAM;
+    return TYPE_WGS84;
+  }
+  
+  public boolean isTransform()
+  {
+    return getType() == TYPE_3PARAM || getType() == TYPE_3PARAM;
+  }
+  
+  public boolean isEqual(Datum datum)
+  {
+    if( getType() != datum.getType()) {
+      return false; // false, datums are not equal
+    } 
+    if( ellipsoid.getEquatorRadius() != ellipsoid.getEquatorRadius()) {
+      if (Math.abs(ellipsoid.getEccentricitySquared() 
+           - datum.ellipsoid.getEccentricitySquared() )  > 0.000000000050)
+      return false;
+    } 
+    if( getType() == TYPE_3PARAM || getType() == TYPE_3PARAM) {
+      for (int i = 0; i < transform.length; i++) {
+        if (transform[i] != datum.transform[i])
+          return false;
+      }
+      return true;
+    } 
+    /* 
+     //TODO: complete
+    else if( this.datum_type == Proj4js.common.PJD_GRIDSHIFT ) {
+      return strcmp( pj_param(this.params,"snadgrids").s,
+                     pj_param(dest.params,"snadgrids").s ) == 0;
+    }
+    */ 
+    return true; // datums are equal
+
+  }
+
+  
+  public void transformFromGeocentricToWgs84(ProjCoordinate p) 
+  {
+    if( transform.length == 3 )
+    {
+      p.x += transform[0];
+      p.y += transform[1];
+      p.z += transform[2];
+
+    }
+    else if (transform.length == 7)
+    {
+      double Dx_BF = transform[0];
+      double Dy_BF = transform[1];
+      double Dz_BF = transform[2];
+      double Rx_BF = transform[3];
+      double Ry_BF = transform[4];
+      double Rz_BF = transform[5];
+      double M_BF  = transform[6];
+      
+      double x_out = M_BF*(       p.x - Rz_BF*p.y + Ry_BF*p.z) + Dx_BF;
+      double y_out = M_BF*( Rz_BF*p.x +       p.y - Rx_BF*p.z) + Dy_BF;
+      double z_out = M_BF*(-Ry_BF*p.x + Rx_BF*p.y +       p.z) + Dz_BF;
+
+      p.x = x_out;
+      p.y = y_out;
+      p.z = z_out;
+    }
+  }
+  public void transformToGeocentricFromWgs84(ProjCoordinate p) 
+  {
+    if( transform.length == 3 )
+    {
+      p.x -= transform[0];
+      p.y -= transform[1];
+      p.z -= transform[2];
+
+    }
+    else if (transform.length == 7)
+    {
+      double Dx_BF = transform[0];
+      double Dy_BF = transform[1];
+      double Dz_BF = transform[2];
+      double Rx_BF = transform[3];
+      double Ry_BF = transform[4];
+      double Rz_BF = transform[5];
+      double M_BF  = transform[6];
+      
+      double x_tmp = (p.x - Dx_BF) / M_BF;
+      double y_tmp = (p.y - Dy_BF) / M_BF;
+      double z_tmp = (p.z - Dz_BF) / M_BF;
+
+      p.x =        x_tmp + Rz_BF*y_tmp - Ry_BF*z_tmp;
+      p.y = -Rz_BF*x_tmp +       y_tmp + Rx_BF*z_tmp;
+      p.z =  Ry_BF*x_tmp - Rx_BF*y_tmp +       z_tmp;
+    }
+  }
+}
