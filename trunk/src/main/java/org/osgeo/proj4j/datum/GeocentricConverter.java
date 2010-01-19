25700b222e5b12ff4d752ce9ba6096f7cb72506b
mdavis <mdavis@4e78687f-474d-0410-85f9-8d5e500ac6b2>
2010-01-19 22:13:31 +0000

    formatting
    git-svn-id: http://svn.osgeo.org/metacrs/proj4j@1769 4e78687f-474d-0410-85f9-8d5e500ac6b2

--- trunk/src/main/java/org/osgeo/proj4j/datum/GeocentricConverter.java
+++ trunk/src/main/java/org/osgeo/proj4j/datum/GeocentricConverter.java
@@ -121,100 +121,100 @@ public class GeocentricConverter
     
   public void convertGeocentricToGeodeticIter(ProjCoordinate p)
   {
-    /* local defintions and variables */
-    /* end-criterium of loop, accuracy of sin(Latitude) */
-    double genau = 1.E-12;
-    double genau2 = (genau*genau);
-    int maxiter = 30;
+  	/* local defintions and variables */
+  	/* end-criterium of loop, accuracy of sin(Latitude) */
+  	double genau = 1.E-12;
+  	double genau2 = (genau*genau);
+  	int maxiter = 30;
 
-    double P;        /* distance between semi-minor axis and location */
-    double RR;       /* distance between center and location */
-    double CT;       /* sin of geocentric latitude */
-    double ST;       /* cos of geocentric latitude */
-    double RX;
-    double RK;
-    double RN;       /* Earth radius at location */
-    double CPHI0;    /* cos of start or old geodetic latitude in iterations */
-    double SPHI0;    /* sin of start or old geodetic latitude in iterations */
-    double CPHI;     /* cos of searched geodetic latitude */
-    double SPHI;     /* sin of searched geodetic latitude */
-    double SDPHI;    /* end-criterium: addition-theorem of sin(Latitude(iter)-Latitude(iter-1)) */
-    boolean At_Pole;     /* indicates location is in polar region */
-        int iter;        /* # of continous iteration, max. 30 is always enough (s.a.) */
+  	double P;        /* distance between semi-minor axis and location */
+  	double RR;       /* distance between center and location */
+  	double CT;       /* sin of geocentric latitude */
+  	double ST;       /* cos of geocentric latitude */
+  	double RX;
+  	double RK;
+  	double RN;       /* Earth radius at location */
+  	double CPHI0;    /* cos of start or old geodetic latitude in iterations */
+  	double SPHI0;    /* sin of start or old geodetic latitude in iterations */
+  	double CPHI;     /* cos of searched geodetic latitude */
+  	double SPHI;     /* sin of searched geodetic latitude */
+  	double SDPHI;    /* end-criterium: addition-theorem of sin(Latitude(iter)-Latitude(iter-1)) */
+  	boolean At_Pole;     /* indicates location is in polar region */
+  	int iter;        /* # of continous iteration, max. 30 is always enough (s.a.) */
 
-        double X = p.x;
-        double Y = p.y;
-        double Z = p.hasValidZOrdinate() ? p.z : 0;   //Z value not always supplied
-        double Longitude;
-        double Latitude;
-        double Height;
+  	double X = p.x;
+  	double Y = p.y;
+  	double Z = p.hasValidZOrdinate() ? p.z : 0;   //Z value not always supplied
+  	double Longitude;
+  	double Latitude;
+  	double Height;
 
-        At_Pole = false;
-        P = Math.sqrt(X*X+Y*Y);
-        RR = Math.sqrt(X*X+Y*Y+Z*Z);
+  	At_Pole = false;
+  	P = Math.sqrt(X*X+Y*Y);
+  	RR = Math.sqrt(X*X+Y*Y+Z*Z);
 
-    /*      special cases for latitude and longitude */
-        if (P/this.a < genau) {
+  	/*      special cases for latitude and longitude */
+  	if (P/this.a < genau) {
 
-    /*  special case, if P=0. (X=0., Y=0.) */
-            At_Pole = true;
-            Longitude = 0.0;
+  		/*  special case, if P=0. (X=0., Y=0.) */
+  		At_Pole = true;
+  		Longitude = 0.0;
 
-    /*  if (X,Y,Z)=(0.,0.,0.) then Height becomes semi-minor axis
-     *  of ellipsoid (=center of mass), Latitude becomes PI/2 */
-            if (RR/this.a < genau) {
-                Latitude = ProjectionMath.HALFPI;
-                Height   = -this.b;
-                return;
-            }
-        } else {
-    /*  ellipsoidal (geodetic) longitude
-     *  interval: -PI < Longitude <= +PI */
-            Longitude=Math.atan2(Y,X);
-        }
+  		/*  if (X,Y,Z)=(0.,0.,0.) then Height becomes semi-minor axis
+  		 *  of ellipsoid (=center of mass), Latitude becomes PI/2 */
+  		if (RR/this.a < genau) {
+  			Latitude = ProjectionMath.HALFPI;
+  			Height   = -this.b;
+  			return;
+  		}
+  	} else {
+  		/*  ellipsoidal (geodetic) longitude
+  		 *  interval: -PI < Longitude <= +PI */
+  		Longitude=Math.atan2(Y,X);
+  	}
 
-    /* --------------------------------------------------------------
-     * Following iterative algorithm was developped by
-     * "Institut für Erdmessung", University of Hannover, July 1988.
-     * Internet: www.ife.uni-hannover.de
-     * Iterative computation of CPHI,SPHI and Height.
-     * Iteration of CPHI and SPHI to 10**-12 radian resp.
-     * 2*10**-7 arcsec.
-     * --------------------------------------------------------------
-     */
-        CT = Z/RR;
-        ST = P/RR;
-        RX = 1.0/Math.sqrt(1.0-this.e2*(2.0-this.e2)*ST*ST);
-        CPHI0 = ST*(1.0-this.e2)*RX;
-        SPHI0 = CT*RX;
-        iter = 0;
+  	/* --------------------------------------------------------------
+  	 * Following iterative algorithm was developped by
+  	 * "Institut für Erdmessung", University of Hannover, July 1988.
+  	 * Internet: www.ife.uni-hannover.de
+  	 * Iterative computation of CPHI,SPHI and Height.
+  	 * Iteration of CPHI and SPHI to 10**-12 radian resp.
+  	 * 2*10**-7 arcsec.
+  	 * --------------------------------------------------------------
+  	 */
+  	CT = Z/RR;
+  	ST = P/RR;
+  	RX = 1.0/Math.sqrt(1.0-this.e2*(2.0-this.e2)*ST*ST);
+  	CPHI0 = ST*(1.0-this.e2)*RX;
+  	SPHI0 = CT*RX;
+  	iter = 0;
 
-    /* loop to find sin(Latitude) resp. Latitude
-     * until |sin(Latitude(iter)-Latitude(iter-1))| < genau */
-        do
-        {
-            iter++;
-            RN = this.a/Math.sqrt(1.0-this.e2*SPHI0*SPHI0);
+  	/* loop to find sin(Latitude) resp. Latitude
+  	 * until |sin(Latitude(iter)-Latitude(iter-1))| < genau */
+  	do
+  	{
+  		iter++;
+  		RN = this.a/Math.sqrt(1.0-this.e2*SPHI0*SPHI0);
 
-    /*  ellipsoidal (geodetic) height */
-            Height = P*CPHI0+Z*SPHI0-RN*(1.0-this.e2*SPHI0*SPHI0);
+  		/*  ellipsoidal (geodetic) height */
+  		Height = P*CPHI0+Z*SPHI0-RN*(1.0-this.e2*SPHI0*SPHI0);
 
-            RK = this.e2*RN/(RN+Height);
-            RX = 1.0/Math.sqrt(1.0-RK*(2.0-RK)*ST*ST);
-            CPHI = ST*(1.0-RK)*RX;
-            SPHI = CT*RX;
-            SDPHI = SPHI*CPHI0-CPHI*SPHI0;
-            CPHI0 = CPHI;
-            SPHI0 = SPHI;
-        }
-        while (SDPHI*SDPHI > genau2 && iter < maxiter);
+  		RK = this.e2*RN/(RN+Height);
+  		RX = 1.0/Math.sqrt(1.0-RK*(2.0-RK)*ST*ST);
+  		CPHI = ST*(1.0-RK)*RX;
+  		SPHI = CT*RX;
+  		SDPHI = SPHI*CPHI0-CPHI*SPHI0;
+  		CPHI0 = CPHI;
+  		SPHI0 = SPHI;
+  	}
+  	while (SDPHI*SDPHI > genau2 && iter < maxiter);
 
-    /*      ellipsoidal (geodetic) latitude */
-        Latitude=Math.atan(SPHI/Math.abs(CPHI));
+  	/*      ellipsoidal (geodetic) latitude */
+  	Latitude=Math.atan(SPHI/Math.abs(CPHI));
 
-        p.x = Longitude;
-        p.y = Latitude;
-        p.z = Height;
+  	p.x = Longitude;
+  	p.y = Latitude;
+  	p.z = Height;
   }
   
   //TODO: port non-iterative algorithm????
