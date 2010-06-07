8575076971b4cea46b6c624f80163f4ad88921ee
mdavis <mdavis@4e78687f-474d-0410-85f9-8d5e500ac6b2>
2010-06-07 00:48:27 +0000

    Added more Ellipsoids
    git-svn-id: http://svn.osgeo.org/metacrs/proj4j@1850 4e78687f-474d-0410-85f9-8d5e500ac6b2

--- trunk/src/main/java/org/osgeo/proj4j/datum/Ellipsoid.java
+++ trunk/src/main/java/org/osgeo/proj4j/datum/Ellipsoid.java
@@ -30,22 +30,147 @@ public class Ellipsoid implements Cloneable {
 	public double eccentricity2 = 1.0;
 
 	// From: USGS PROJ package.
-	public final static Ellipsoid SPHERE = new Ellipsoid("sphere", 6371008.7714, 6371008.7714, 0.0, "Sphere");
-	public final static Ellipsoid BESSEL = new Ellipsoid("bessel", 6377397.155, 0.0, 299.1528128, "Bessel 1841");
-	public final static Ellipsoid CLARKE_1866 = new Ellipsoid("clrk66", 6378206.4, 6356583.8, 0.0, "Clarke 1866");
-	public final static Ellipsoid CLARKE_1880 = new Ellipsoid("clrk80", 6378249.145, 0.0, 293.4663, "Clarke 1880 mod.");
-  public final static Ellipsoid AIRY = new Ellipsoid("airy", 6377563.396, 6356256.910, 0.0, "Airy 1830");
-  public final static Ellipsoid MOD_AIRY = new Ellipsoid("airy", 6377340.189, 6356034.446, 0.0, "Modified Airy");
-	public final static Ellipsoid WGS60 = new Ellipsoid("WGS60", 6378165.0, 0.0, 298.3, "WGS 60");
-	public final static Ellipsoid WGS66 = new Ellipsoid("WGS66", 6378145.0, 0.0, 298.25, "WGS 66");
-	public final static Ellipsoid WGS72 = new Ellipsoid("WGS72", 6378135.0, 0.0, 298.26, "WGS 72");
-	public final static Ellipsoid WGS84 = new Ellipsoid("WGS84", 6378137.0, 0.0, 298.257223563, "WGS 84");
-	public final static Ellipsoid KRASOVSKY = new Ellipsoid("krass", 6378245.0, 298.3, 0.0, "Krassovsky, 1942");
-	public final static Ellipsoid EVEREST = new Ellipsoid("evrst30", 6377276.345, 0.0, 300.8017, "Everest 1830");
   public final static Ellipsoid INTERNATIONAL = new Ellipsoid("intl", 6378388.0, 0.0, 297.0, "International 1909 (Hayford)");
-  public final static Ellipsoid INTERNATIONAL_1967 = new Ellipsoid("new_intl", 6378157.5, 6356772.2, 0.0, "New International 1967");
-	public final static Ellipsoid GRS80 = new Ellipsoid("GRS80", 6378137.0, 0.0, 298.257222101, "GRS 1980 (IUGG, 1980)");
-	public final static Ellipsoid AUSTRALIAN = new Ellipsoid("australian", 6378160.0, 6356774.7, 298.25, "Australian");
+
+  public final static Ellipsoid BESSEL
+  = new Ellipsoid("bessel", 6377397.155, 0.0, 299.1528128, "Bessel 1841");
+public final static Ellipsoid CLARKE_1866
+  = new Ellipsoid("clrk66", 6378206.4, 6356583.8, 0.0, "Clarke 1866");
+public final static Ellipsoid CLARKE_1880
+  = new Ellipsoid("clrk80", 6378249.145, 0.0, 293.4663, "Clarke 1880 mod.");
+public final static Ellipsoid AIRY
+  = new Ellipsoid("airy", 6377563.396, 6356256.910, 0.0, "Airy 1830");
+public final static Ellipsoid WGS60
+  = new Ellipsoid("WGS60", 6378165.0, 0.0, 298.3, "WGS 60");
+public final static Ellipsoid WGS66
+  = new Ellipsoid("WGS66", 6378145.0, 0.0, 298.25, "WGS 66");
+public final static Ellipsoid WGS72
+  = new Ellipsoid("WGS72", 6378135.0, 0.0, 298.26, "WGS 72");
+public final static Ellipsoid WGS84
+  = new Ellipsoid("WGS84", 6378137.0, 0.0, 298.257223563, "WGS 84");
+public final static Ellipsoid KRASOVSKY
+  = new Ellipsoid("krass", 6378245.0, 298.3, 0.0, "Krassovsky, 1942");
+public final static Ellipsoid EVEREST
+  = new Ellipsoid("evrst30", 6377276.345, 0.0, 300.8017, "Everest 1830");
+public final static Ellipsoid INTERNATIONAL_1967
+  = new Ellipsoid("new_intl", 6378157.5, 6356772.2, 0.0, "New International 1967");
+public final static Ellipsoid GRS80
+  = new Ellipsoid("GRS80", 6378137.0, 0.0, 298.257222101, "GRS 1980 (IUGG, 1980)");
+public final static Ellipsoid AUSTRALIAN
+  = new Ellipsoid("australian", 6378160.0, 6356774.7, 298.25, "Australian");
+public final static Ellipsoid MERIT 
+  = new Ellipsoid("MERIT", 6378137.0, 0.0, 298.257, "MERIT 1983");
+public final static Ellipsoid SGS85
+  = new Ellipsoid("SGS85", 6378136.0, 0.0, 298.257, "Soviet Geodetic System 85");
+public final static Ellipsoid IAU76
+  = new Ellipsoid("IAU76", 6378140.0, 0.0, 298.257, "IAU 1976");
+public final static Ellipsoid APL4_9
+  = new Ellipsoid("APL4.9", 6378137.0, 0.0, 298.25, "Appl. Physics. 1965");
+public final static Ellipsoid NWL9D
+  = new Ellipsoid("NWL9D", 6378145.0, 298.25, 0.0, "Naval Weapons Lab., 1965");
+public final static Ellipsoid MOD_AIRY
+  = new Ellipsoid("mod_airy", 6377340.189, 6356034.446, 0.0, "Modified Airy");
+public final static Ellipsoid ANDRAE
+  = new Ellipsoid("andrae", 6377104.43, 300.0, 0.0, "Andrae 1876 (Den., Iclnd.)");
+public final static Ellipsoid AUST_SA
+  = new Ellipsoid("aust_SA", 6378160.0, 0.0, 298.25, "Australian Natl & S. Amer. 1969");
+public final static Ellipsoid GRS67
+  = new Ellipsoid("GRS67", 6378160.0, 0.0, 298.2471674270, "GRS 67 (IUGG 1967)");
+public final static Ellipsoid BESS_NAM
+  = new Ellipsoid("bess_nam", 6377483.865, 0.0, 299.1528128, "Bessel 1841 (Namibia)");
+public final static Ellipsoid CPM
+  = new Ellipsoid("CPM", 6375738.7, 0.0, 334.29, "Comm. des Poids et Mesures 1799");
+public final static Ellipsoid DELMBR
+  = new Ellipsoid("delmbr", 6376428.0, 0.0, 311.5, "Delambre 1810 (Belgium)");
+public final static Ellipsoid ENGELIS
+   = new Ellipsoid("engelis", 6378136.05, 0.0, 298.2566, "Engelis 1985");
+public final static Ellipsoid EVRST48
+   = new Ellipsoid("evrst48", 6377304.063, 0.0, 300.8017, "Everest 1948");
+public final static Ellipsoid EVRST56
+   = new Ellipsoid("evrst56", 6377301.243, 0.0, 300.8017, "Everest 1956");
+public final static Ellipsoid EVRTS69
+   = new Ellipsoid("evrst69", 6377295.664, 0.0, 300.8017, "Everest 1969");
+public final static Ellipsoid EVRTSTSS
+   = new Ellipsoid("evrstSS", 6377298.556, 0.0, 300.8017, "Everest (Sabah & Sarawak)");
+public final static Ellipsoid FRSCH60
+   = new Ellipsoid("fschr60", 6378166.0, 0.0, 298.3, "Fischer (Mercury Datum) 1960");
+public final static Ellipsoid FSRCH60M
+   = new Ellipsoid("fschr60m", 6378155.0, 0.0, 298.3, "Modified Fischer 1960");
+public final static Ellipsoid FSCHR68
+   = new Ellipsoid("fschr68", 6378150.0, 0.0, 298.3, "Fischer 1968");
+public final static Ellipsoid HELMERT
+   = new Ellipsoid("helmert", 6378200.0, 0.0, 298.3, "Helmert 1906");
+public final static Ellipsoid HOUGH
+   = new Ellipsoid("hough", 6378270.0, 0.0, 297.0, "Hough");
+public final static Ellipsoid INTL
+   = new Ellipsoid("intl", 6378388.0, 0.0, 297.0, "International 1909 (Hayford)");
+public final static Ellipsoid KAULA
+   = new Ellipsoid("kaula", 6378163.0, 0.0, 298.24, "Kaula 1961");
+public final static Ellipsoid LERCH
+   = new Ellipsoid("lerch", 6378139.0, 0.0, 298.257, "Lerch 1979");
+public final static Ellipsoid MPRTS
+   = new Ellipsoid("mprts", 6397300.0, 0.0, 191.0, "Maupertius 1738");
+public final static Ellipsoid PLESSIS
+   = new Ellipsoid("plessis", 6376523.0, 6355863.0, 0.0, "Plessis 1817 France)");
+public final static Ellipsoid SEASIA
+   = new Ellipsoid("SEasia", 6378155.0, 6356773.3205, 0.0, "Southeast Asia");
+public final static Ellipsoid WALBECK
+   = new Ellipsoid("walbeck", 6376896.0, 6355834.8467, 0.0, "Walbeck");
+public final static Ellipsoid NAD27
+   = new Ellipsoid("NAD27", 6378249.145, 0.0, 293.4663, "NAD27: Clarke 1880 mod.");
+public final static Ellipsoid NAD83
+   = new Ellipsoid("NAD83", 6378137.0, 0.0, 298.257222101, "NAD83: GRS 1980 (IUGG, 1980)");
+public final static Ellipsoid SPHERE
+   = new Ellipsoid("sphere", 6371008.7714 , 6371008.7714, 0.0, "Sphere");
+
+
+  public final static Ellipsoid[] ellipsoids = {
+    BESSEL,
+    CLARKE_1866,
+    CLARKE_1880,
+    AIRY,
+    WGS60,
+    WGS66,
+    WGS72,
+    WGS84,
+    KRASOVSKY,
+    EVEREST,
+    INTERNATIONAL_1967,
+    GRS80,
+    AUSTRALIAN,
+    MERIT ,
+    SGS85,
+    IAU76,
+    APL4_9,
+    NWL9D,
+    MOD_AIRY,
+    ANDRAE,
+    AUST_SA,
+    GRS67,
+    BESS_NAM,
+    CPM,
+    DELMBR,
+    ENGELIS,
+    EVRST48,
+    EVRST56,
+    EVRTS69,
+    EVRTSTSS,
+    FRSCH60,
+    FSRCH60M,
+    FSCHR68,
+    HELMERT,
+    HOUGH,
+    INTL,
+    KAULA,
+    LERCH,
+    MPRTS,
+    PLESSIS,
+    SEASIA,
+    WALBECK,
+    NAD27,
+    NAD83,
+    SPHERE
+    };
 	
 
 	public Ellipsoid() {
